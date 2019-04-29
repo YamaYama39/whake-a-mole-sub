89e308bb2c994f4fa8f82642c5302990a49ed907
YamaYama39 <45874566+YamaYama39@users.noreply.github.com>
2019-04-29 10:18:28 +0900

    Add files via upload

---
+++ GameMain.cpp
@@ -0,0 +1,25 @@
+﻿#include "GameMain.h"
+#include <Siv3D.hpp>
+
+
+GameMain::GameMain() {
+	if (!init()) {
+		//error
+	}
+}
+
+bool GameMain::init() {
+	return true;
+}
+
+void GameMain::execute() {
+		Graphics::SetBackground(ColorF(0.8, 0.9, 1.0));
+		while (System::Update()){
+
+		}
+	finish();
+}
+
+void GameMain::finish() {
+
+}
---
+++ GameMain.h
@@ -0,0 +1,12 @@
+#pragma once
+
+class GameMain {
+private:
+	bool init();
+	void finish();
+
+public:
+	GameMain();
+
+	void execute();
+};
